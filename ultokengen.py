6a2e967b6481287221f064d047ee8cea9bb0dde1
jj358mhz <jj358mhz@gmail.com>
2014-04-22 14:35:52 -0700

    Update ultokengen.py
    Amended the prompt text for cid parameter to ask for GUID

--- ultokengen.py
+++ ultokengen.py
@@ -12,7 +12,7 @@ pbURL = raw_input('Enter the Playback URL : ') # the playback URL from the upLyn
 print ("%s, interesting URL" % pbURL);
 ct = raw_input('Enter the content type, a for asset or c for live channel : ') # the content type
 print ("%s. Cool" % ct);
-cid = raw_input('Enter the content ID : ') # the content ID from the upLynk CMS
+cid = raw_input('Enter the GUID : ') # the content ID from the upLynk CMS
 print ("Wow, %s is a big number!" % cid);
 rays =raw_input('Enter the customization parameters, abc : ') # customization parameter
 print ("With this %s, I can begin to compute" % rays);
