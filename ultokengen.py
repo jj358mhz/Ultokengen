2bc29263962ee02e9d9470a6bbba2b848d550bb7
jj358mhz <jj358mhz@gmail.com>
2014-05-30 22:46:02 -0700

    Initial commit

--- ultokengen.py
+++ ultokengen.py
@@ -5,18 +5,12 @@
 import hashlib, time, hmac, urllib
 
 # inputs
-ip = raw_input('Enter your IP address : ') # the user's IP address; normally obtained from the server itself
-print ("Hi %s, I know where you live!" % ip);
+ip = raw_input('Enter your IP address : ') # the user's IP address
 apiKey = raw_input('Enter your API key : ') # from the CMS UI
-print ("Shhhhh, be sure to keep %s secret!" % apiKey);
 pbURL = raw_input('Enter the Playback URL : ') # the playback URL from the upLynk CMS
-print ("%s, interesting URL" % pbURL);
 ct = raw_input('Enter the content type, a for asset or c for live channel : ') # the content type
-print ("%s. Cool" % ct);
 cid = raw_input('Enter the GUID : ') # the content ID from the upLynk CMS
-print ("Wow, %s is a big number!" % cid);
 rays = raw_input('Enter the customization parameters, abc : ') # customization parameter
-print ("With this %s, I can begin to compute" % rays);
 
 # combine all of the parameters except the signature
 queryStr = urllib.urlencode(dict(
