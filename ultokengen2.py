1dd8349fe2eddb25c4933fa52f56676201cd7353
jj358mhz <masaki69>
2014-11-29 15:33:52 -0800

    Initial commit

---
+++ ultokengen2.py
@@ -0,0 +1,32 @@
+__author__ = 'v402625'
+
+import hashlib
+import time
+import hmac
+import urllib
+import random
+
+# inputs
+apiKey = raw_input('Enter your API key : ')  # from the CMS UI
+url = raw_input('Enter the Playback URL : ')  # the playback URL from the upLynk CMS
+ct = raw_input('Enter the content type, "a" for asset or "c" for live channel : ')  # the content type
+cid = raw_input('Enter the GUID : ')  # the content ID from the upLynk CMS
+#rays = raw_input('Enter the customization parameters, abc : ')  # customization parameter
+
+# combine all of the parameters except the signature
+queryStr = urllib.urlencode(dict(
+    tc='1',  # token check algorithm version
+    exp=int(time.time()) + 1577836800,  # expire 50 years from now
+    rn=str(random.randint(0, 2**32)),  # random number
+    ct=ct,  # an asset
+    cid=cid,  # the asset's ID
+    #rays=rays,  # customization parameter
+))
+
+# compute the signature and add it to the *end*
+sig = hmac.new(apiKey, queryStr, hashlib.sha256).hexdigest()
+queryStr = queryStr + '&sig=' + sig
+
+# The token would then be added to a playback URL, e.g.
+turl = url + '?' + queryStr
+print turl
