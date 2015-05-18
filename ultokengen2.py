cec85856db026781270b45db9dbda6f37c431335
jj358mhz <masaki69>
2015-05-18 09:54:36 -0700

    Added code to format URL to be XML-compliant

--- ultokengen2.py
+++ ultokengen2.py
@@ -16,7 +16,7 @@ cid = raw_input('Enter the GUID : ')  # the content ID from the upLynk CMS
 # combine all of the parameters except the signature
 queryStr = urllib.urlencode(dict(
     tc='1',  # token check algorithm version
-    exp=int(time.time()) + 1577836800,  # expire 50 years from now
+    exp=int(time.time()) + 3136320000,  # expire 100 years from now
     rn=str(random.randint(0, 2**32)),  # random number
     ct=ct,  # an asset
     cid=cid,  # the asset's ID
@@ -29,4 +29,9 @@ queryStr = queryStr + '&sig=' + sig
 
 # The token would then be added to a playback URL, e.g.
 turl = url + '?' + queryStr
-print turl
+print ("Standard URL: "+turl)
+
+# The following code provides a XML-compliant URL
+new_string = turl.replace('&', '&#38;')
+final_url = new_string.replace('?', '&#63;')
+print ("XML-compliant URL: "+final_url)
